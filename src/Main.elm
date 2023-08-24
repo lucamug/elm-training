2d757c0a2881b3fd978200af69fe6f5345698a7a
Luca <lucamug@gmail.com>
2023-08-24 10:26:40 +0900

    Session August 23, 2023

--- src/Main.elm
+++ src/Main.elm
@@ -1,51 +1,73 @@
-module Main exposing (main)
+module Main exposing (..)
+
+-- Press buttons to increment and decrement a counter.
+--
+-- Read how it works:
+--   https://guide.elm-lang.org/architecture/buttons.html
+--
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (..)
+import Html.Events exposing (..)
 
 
 
--- See also https://ellie-app.com/new
+-- MAIN
+
+
+main =
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
 
 
 type alias Model =
-    { count : Int }
+    { counter : Int
+    , input : String
+    }
 
 
-initialModel : Model
-initialModel =
-    { count = 0 }
+
+-- MODEL
+
+
+init : Model
+init =
+    { counter = 0
+    , input = ""
+    }
+
+
+
+-- UPDATE
 
 
 type Msg
     = Increment
     | Decrement
+    | NewText String
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         Increment ->
-            { model | count = model.count + 1 }
+            { model | counter = model.counter + 1 }
 
         Decrement ->
-            { model | count = model.count - 1 }
+            { model | counter = model.counter - 1 }
+
+        NewText input ->
+            { model | input = input }
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick Increment ] [ text "+1" ]
-        , div [] [ text <| String.fromInt model.count ]
-        , button [ onClick Decrement ] [ text "-1" ]
+        [ button [ onClick Decrement ] [ text "-" ]
+        , input [ onInput NewText ] [ text "-" ]
+        , div [] [ text (Debug.toString model) ]
+        , button [ onClick Increment ] [ text "+" ]
         ]
-
-
-main : Program () Model Msg
-main =
-    Browser.sandbox
-        { init = initialModel
-        , view = view
-        , update = update
-        }
