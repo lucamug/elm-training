232354827078869e3f540bd8a83b2c2e3e565a9d
Luca <lucamug@gmail.com>
2023-08-24 18:08:32 +0900

    TODOs

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,17 @@
 module Main exposing (..)
 
--- Press buttons to increment and decrement a counter.
---
--- Read how it works:
---   https://guide.elm-lang.org/architecture/buttons.html
---
-
 import Browser
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Json.Decode
 
 
 
 -- MAIN
 
 
+main : Program () Model Msg
 main =
     Browser.sandbox
         { init = init
@@ -23,9 +20,18 @@ main =
         }
 
 
+type alias Todo =
+    { deleted : Bool
+    , done : Bool
+    , descr : String
+    , id : Int
+    }
+
+
 type alias Model =
-    { counter : Int
-    , input : String
+    { input : String
+    , todos : List Todo
+    , latestId : Int
     }
 
 
@@ -35,8 +41,9 @@ type alias Model =
 
 init : Model
 init =
-    { counter = 0
-    , input = ""
+    { input = ""
+    , todos = []
+    , latestId = 0
     }
 
 
@@ -45,29 +52,127 @@ init =
 
 
 type Msg
-    = Increment
-    | Decrement
-    | NewText String
+    = NewText String
+    | AddTodo
+    | Delete Int
+    | Undelete Int
+    | Done Int
+    | Undone Int
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Increment ->
-            { model | counter = model.counter + 1 }
-
-        Decrement ->
-            { model | counter = model.counter - 1 }
-
         NewText input ->
             { model | input = input }
+
+        AddTodo ->
+            { model
+                | todos =
+                    { deleted = False
+                    , done = False
+                    , descr = model.input
+                    , id = model.latestId + 1
+                    }
+                        :: model.todos
+                , input = ""
+                , latestId = model.latestId + 1
+            }
+
+        Delete id ->
+            updateTodo (\todo -> { todo | deleted = True }) id model
+
+        Done id ->
+            updateTodo (\todo -> { todo | done = True }) id model
+
+        Undelete id ->
+            updateTodo (\todo -> { todo | deleted = False }) id model
+
+        Undone id ->
+            updateTodo (\todo -> { todo | done = False }) id model
+
+
+updateTodo : (Todo -> Todo) -> Int -> Model -> Model
+updateTodo mapTodo id model =
+    { model
+        | todos =
+            List.map
+                (\todo ->
+                    if id == todo.id then
+                        mapTodo todo
+
+                    else
+                        todo
+                )
+                model.todos
+    }
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , input [ onInput NewText ] [ text "-" ]
-        , div [] [ text (Debug.toString model) ]
-        , button [ onClick Increment ] [ text "+" ]
-        ]
+        ([ div []
+            [ input
+                [ onInput NewText
+                , value model.input
+                , onEnter AddTodo
+                ]
+                [ text "-" ]
+            , button [ onClick AddTodo ] [ text "Add TODO" ]
+            ]
+         ]
+            ++ viewHelper "TODOs"
+                (\todo -> not todo.deleted && not todo.done)
+                model.todos
+                (\id ->
+                    [ button [ onClick (Delete id) ] [ text "Delete" ]
+                    , button [ onClick (Done id) ] [ text "Done" ]
+                    ]
+                )
+            ++ viewHelper "Done"
+                .done
+                model.todos
+                (\id ->
+                    [ button [ onClick (Undone id) ] [ text "Undone" ] ]
+                )
+            ++ viewHelper "Deleted"
+                .deleted
+                model.todos
+                (\id ->
+                    [ button [ onClick (Undelete id) ] [ text "Undelete" ] ]
+                )
+        )
+
+
+viewHelper : String -> (Todo -> Bool) -> List Todo -> (Int -> List (Html msg)) -> List (Html msg)
+viewHelper title filter todos elements =
+    [ h2 [] [ text title ]
+    , div []
+        (List.map
+            (\todo ->
+                div []
+                    (elements todo.id
+                        ++ [ text (String.fromInt todo.id)
+                           , text " - "
+                           , text todo.descr
+                           ]
+                    )
+            )
+            (List.filter filter todos)
+        )
+    ]
+
+
+onEnter : Msg -> Attribute Msg
+onEnter msg =
+    on "keydown"
+        (Json.Decode.andThen
+            (\code ->
+                if code == 13 then
+                    Json.Decode.succeed msg
+
+                else
+                    Json.Decode.fail "not ENTER"
+            )
+            keyCode
+        )
