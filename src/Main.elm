b3c2d17670ab3b17f1ddb42f7003ba59b6c2c45f
Luca <lucamug@gmail.com>
2024-05-16 14:45:04 +0900

    Convert to elm-ui

--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,16 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, input, p, text)
+import Element exposing (..)
+import Element.Border as Border
+import Element.Input as Input
+import Html exposing (Html)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
 
 type alias Model =
     { count : Int
-    , count2 : Int
     , inputValue : String
     , msg : Msg
     }
@@ -17,7 +19,6 @@ type alias Model =
 init : Model
 init =
     { count = 10
-    , count2 = 20
     , inputValue = "Initial value"
     , msg = Increment
     }
@@ -26,8 +27,6 @@ init =
 type Msg
     = Increment
     | Decrement
-    | Increment2
-    | Decrement2
     | GotNewText String
 
 
@@ -40,24 +39,8 @@ update msg model =
         Decrement ->
             { model | count = model.count - 1 }
 
-        Increment2 ->
-            { model | count2 = model.count2 + 1 }
-
-        Decrement2 ->
-            { model | count2 = model.count2 - 1 }
-
         GotNewText newText ->
             { model | inputValue = newText }
-
-
-f : Msg
-f =
-    Increment
-
-
-g : String -> Msg
-g =
-    GotNewText
 
 
 
@@ -67,23 +50,30 @@ g =
 
 view : Model -> Html Msg
 view model =
-    div [ style "margin" "40px" ]
-        [ button [ onClick Increment ] [ text "Increment" ]
-        , input [ onInput GotNewText, value model.inputValue ] []
-        , p [] [ text <| String.fromInt model.count ]
-        , button [ onClick Decrement ] [ text "Decrement" ]
-        , button [ onClick Increment2 ] [ text "Increment" ]
-        , p [] [ text <| String.fromInt model.count2 ]
-        , button [ onClick Decrement2 ]
-            [ text
-                (if False then
-                    "Decrement"
-
-                 else
-                    ""
-                )
+    layout [ padding 40 ] <|
+        column [ spacing 20 ]
+            [ Input.button [ padding 20, Border.width 1 ] { label = text "Increment", onPress = Just Increment }
+            , text <| String.fromInt model.count
             ]
-        ]
+
+
+
+-- [ button [ onClick Increment ] [ text "Increment" ]
+-- , input [ onInput GotNewText, value model.inputValue ] []
+-- , p [] [ text <| String.fromInt model.count ]
+-- , button [ onClick Decrement ] [ text "Decrement" ]
+-- , button [ onClick Increment2 ] [ text "Increment" ]
+-- , p [] [ text <| String.fromInt model.count2 ]
+-- , button [ onClick Decrement2 ]
+--     [ text
+--         (if False then
+--             "Decrement"
+--
+--          else
+--             ""
+--         )
+--     ]
+-- ]
 
 
 main : Program () Model Msg
