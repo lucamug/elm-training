2d491c4a2a0c24d6f6432718b9f455dfd83bb160
Luca <lucamug@gmail.com>
2025-04-23 17:35:08 +0900

    Session April 23rd, 2025

--- src/Main.elm
+++ src/Main.elm
@@ -1,66 +1,327 @@
 module Main exposing (main)
 
 import Browser
-import Element
-import Html exposing (Html, button, div, p, text)
-import Html.Attributes exposing (..)
-import Html.Events exposing (..)
+import Browser.Events
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
+import Element.Input as Input
+import Html
+import Html.Attributes
+
+
+
+-- 💥 🪐 👽 ▶️ ⏸️ 🛸
+
+
+type State
+    = Playing
+    | Over
+    | Paused
+    | Won
+
+
+type Direction
+    = Left
+    | Right
 
 
 type alias Model =
-    { count : Int
+    { points : Int
+    , positionX : Float
+    , positionY : Float
+    , direction : Direction
+    , state : State
+    , aliens : List ( Float, Float )
     }
+
+
+init : ( Model, Cmd msg )
+init =
+    ( { points = 10
+      , positionX = 0
+      , positionY = 0
+      , direction = Right
+      , state = Playing
+      , aliens = []
+      }
+    , Cmd.none
+    )
 
 
 type Msg
     = Increment
-    | Decrement
+    | Fire
+    | OnAnimationFrame Float
+    | TogglePause
 
 
-init : Model
-init =
-    { count = 10 }
+speedX : Float
+speedX =
+    5
 
 
+speedY : Float
+speedY =
+    10
+
+
+speedAlien : Float
+speedAlien =
+    4
+
+
+update : Msg -> Model -> ( Model, Cmd msg )
 update msg model =
     case msg of
         Increment ->
-            { model | count = model.count + 1 }
+            ( { model | points = model.points + 1 }, Cmd.none )
 
-        Decrement ->
-            { model | count = model.count - 1 }
-
-
-sum a b =
-    a + b + 0.1
-
-
-view model =
-    let
-        _ =
-            Debug.log "xxx" model
-    in
-    div [ style "margin" "40px" ]
-        -- div : List (Attribute msg) -> List (Html msg) -> Html msg
-        -- button : List (Attribute msg) -> List (Html msg) -> Html msg
-        -- onClick : msg -> Attribute msg
-        -- text : String -> Html msg
-        [ button [ onClick Increment ] [ text "Increment" ]
-        , p [] [ text (String.fromInt model.count) ]
-        , button [ onClick Decrement ] [ text "Decrement" ]
-        , Element.layout []
-            (Element.row [ Element.spacing 10 ]
-                [ Element.text "Hi"
-                , Element.text "Hi"
-                ]
+        Fire ->
+            ( { model
+                | aliens = ( model.positionX, model.positionY ) :: model.aliens
+              }
+            , Cmd.none
             )
-        ]
+
+        OnAnimationFrame delta ->
+            let
+                newPoints =
+                    model.aliens
+                        |> List.map
+                            (\( alienX, alienY ) -> ( alienX, alienY + speedAlien ))
+                        |> List.filter
+                            (\( alienX, alienY ) -> hitTargetEnd ( alienX, alienY ))
+                        |> List.length
+                        |> (\qtyAliensThatHitTarget -> model.points - qtyAliensThatHitTarget)
+            in
+            ( { model
+                | positionX =
+                    case model.direction of
+                        Left ->
+                            model.positionX - speedX
+
+                        Right ->
+                            model.positionX + speedX
+                , positionY =
+                    if model.positionX > 270 || model.positionX < 0 then
+                        model.positionY + speedY
+
+                    else
+                        model.positionY
+                , direction =
+                    if model.positionX > 270 then
+                        Left
+
+                    else if model.positionX < 0 then
+                        Right
+
+                    else
+                        model.direction
+                , aliens =
+                    model.aliens
+                        |> List.map
+                            (\( alienX, alienY ) -> ( alienX, alienY + speedAlien ))
+                        |> List.filter
+                            (\( _, alienY ) -> alienY < 500)
+                        |> List.filter
+                            (\( alienX, alienY ) -> not (hitTargetEnd ( alienX, alienY )))
+                , points =
+                    newPoints
+                , state =
+                    if newPoints == 0 then
+                        Won
+
+                    else if model.positionY == 330 && model.positionX < 185 then
+                        Over
+
+                    else
+                        model.state
+              }
+            , Cmd.none
+            )
+
+        TogglePause ->
+            ( { model
+                | state =
+                    case model.state of
+                        Playing ->
+                            Paused
+
+                        Over ->
+                            Over
+
+                        Paused ->
+                            Playing
+
+                        Won ->
+                            Won
+              }
+            , Cmd.none
+            )
+
+
+hitTargetStart : ( Float, Float ) -> Bool
+hitTargetStart ( alienX, alienY ) =
+    alienY > 320 && within { min = 100, max = 200 } ( alienX, alienY )
+
+
+hitTargetEnd : ( Float, Float ) -> Bool
+hitTargetEnd ( alienX, alienY ) =
+    alienY > 370 && within { min = 100, max = 200 } ( alienX, alienY )
+
+
+within : { min : Float, max : Float } -> ( Float, Float ) -> Bool
+within { min, max } ( alienX, _ ) =
+    min < alienX && alienX < max
+
+
+attrsButton : List (Attribute msg)
+attrsButton =
+    [ Border.width 1
+    , paddingXY 20 10
+    , Border.rounded 100
+    , Background.color <| rgb 1 1 1
+    , Font.size 40
+    ]
+
+
+view : Model -> Html.Html Msg
+view model =
+    layout [ padding 20, Background.color <| rgb 0 0.4 0.6 ] <|
+        column
+            ([ spacing 20
+             ]
+                ++ [ inFront <|
+                        el
+                            [ Font.size 150
+                            , moveDown 450
+                            , moveRight 100
+                            ]
+                        <|
+                            text "🪐"
+                   ]
+                ++ List.map
+                    (\( alienX, alienY ) ->
+                        inFront <|
+                            el
+                                [ Font.size 30
+                                , moveRight (alienX + 30)
+                                , moveDown (alienY + 120)
+                                ]
+                            <|
+                                text
+                                    (if hitTargetStart ( alienX, alienY ) then
+                                        "💥"
+
+                                     else
+                                        "👽"
+                                    )
+                    )
+                    model.aliens
+                ++ (if model.state == Over then
+                        [ inFront <|
+                            el
+                                (attrsButton
+                                    ++ [ moveDown 200
+                                       , moveRight 50
+                                       ]
+                                )
+                            <|
+                                text "Game Over!"
+                        ]
+
+                    else if model.state == Won then
+                        [ inFront <|
+                            el
+                                (attrsButton
+                                    ++ [ moveDown 200
+                                       , moveRight 50
+                                       ]
+                                )
+                            <|
+                                text "You Win!"
+                        ]
+
+                    else
+                        []
+                   )
+             -- ++ [ inFront <|
+             --         paragraph
+             --             [ moveDown 100, Font.color <| rgb 1 1 1 ]
+             --             [ text <|
+             --                 Debug.toString model
+             --             ]
+             --    ]
+            )
+            [ row [ spacing 40 ]
+                [ Input.button attrsButton
+                    { onPress = Just Fire
+                    , label = text "👽"
+                    }
+                , Input.button attrsButton
+                    { onPress = Just TogglePause
+                    , label =
+                        text <|
+                            case model.state of
+                                Playing ->
+                                    "⏸️"
+
+                                Over ->
+                                    "-"
+
+                                Paused ->
+                                    "▶️"
+
+                                Won ->
+                                    "-"
+                    }
+                , el attrsButton <| text <| String.fromInt model.points
+                ]
+            , Input.button
+                [ moveRight model.positionX
+                , moveDown model.positionY
+                , Font.size 100
+                , case model.direction of
+                    Left ->
+                        rotate 0
+
+                    Right ->
+                        rotate 0.6
+                , htmlAttribute <| Html.Attributes.style "transition" "transform 100ms"
+                ]
+                { onPress = Just Increment
+                , label =
+                    text
+                        (if model.state == Over then
+                            "💥"
+
+                         else
+                            "🛸"
+                        )
+                }
+            ]
 
 
 main : Program () Model Msg
 main =
-    Browser.sandbox
-        { init = init
+    Browser.element
+        { init = \_ -> init
         , view = view
         , update = update
+        , subscriptions =
+            \model ->
+                case model.state of
+                    Playing ->
+                        Browser.Events.onAnimationFrameDelta OnAnimationFrame
+
+                    Over ->
+                        Sub.none
+
+                    Paused ->
+                        Sub.none
+
+                    Won ->
+                        Sub.none
         }
